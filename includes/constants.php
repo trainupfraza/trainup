1531d56601fbcdaea1feb0005daae3fda4d7906a
comree <franciszaunda@gmail.com>
2025-11-03 05:44:36 +0200

    Final fix i guess

--- includes/constants.php
+++ includes/constants.php
@@ -1,14 +1,14 @@
 <?php
 // Database Constants
-define('DB_SERVER', 'sql8.freesqldatabase.com');
-define('DB_USERNAME', 'sql8805800');
-define('DB_PASSWORD', 'WpKfZ6vfYc'); // You need to add your actual password
+define('DB_HOST', 'sql8.freesqldatabase.com');
+define('DB_USER', 'sql8805800');
+define('DB_PASSWORD', 'WpKfZ6vfYc');
 define('DB_NAME', 'sql8805800');
 define('DB_PORT', 3306);
 
 // Attempt to connect to MySQL database
 try {
-    $pdo = new PDO("mysql:host=" . DB_SERVER . ";port=" . DB_PORT . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
