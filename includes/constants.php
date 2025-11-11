ef2c32a03dba5bbcc06dd9b1b5ab32fae3582fbb
trainupfraza <franciszaunda@outlook.com>
2025-11-11 04:20:35 +0200

    Update constants.php

--- includes/constants.php
+++ includes/constants.php
@@ -1,14 +1,14 @@
 <?php
-// Database Constants
-define('DB_HOST', 'sql8.freesqldatabase.com');
-define('DB_USER', 'sql8805800');
-define('DB_PASSWORD', 'WpKfZ6vfYc');
-define('DB_NAME', 'sql8805800');
-define('DB_PORT', 3306);
+// Database Constants - PostgreSQL Render
+define('DB_HOST', 'dpg-d4968oc9c44c73bgq220-a.oregon-postgres.render.com');
+define('DB_USER', 'train_up_user');
+define('DB_PASSWORD', '6sebZERlRZWaxbVRTVWf67nnARKXtDVe');
+define('DB_NAME', 'train_up_db_m8dc');
+define('DB_PORT', 5432);
 
-// Attempt to connect to MySQL database
+// Attempt to connect to PostgreSQL database
 try {
-    $pdo = new PDO("mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+    $pdo = new PDO("pgsql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
